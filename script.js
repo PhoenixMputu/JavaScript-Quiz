59864079f665f8d3339c4e6350184136206648c8
Victory Deo Tshimbadi <victory23tshimbadi@gmail.com>
2022-05-30 15:18:47 +0200

    Accueil Screen

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-(() => {
-  'use strict'
+let main = document.getElementById('main');
+let second = document.getElementById('second');
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll('.needs-validation')
+let nom = "";
+let email = "";
 
-  // Loop over them and prevent submission
-  Array.from(forms).forEach(form => {
-    form.addEventListener('submit', event => {
-      if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-      }
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
 
-      form.classList.add('was-validated')
-    }, false)
-  })
-})()
+    let erreurName = document.querySelector('.back');
+    let erreurEmail = document.querySelector('.backs');
+
+    if (this.elements[0].value == "" && this.elements[1].value == "") {
+        erreurName.textContent = "Veuillez saisir votre nom !";
+        erreurEmail.textContent = 'Veuillez saisir votre email !';
+    } else {
+        erreurName.textContent = "";
+        let regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!regexEmail.test(this.elements[1].value)) {
+            erreurEmail.textContent = 'Veuillez saisir votre email !';
+        } else {
+            question = fetch("main.test")
+            erreurEmail.textContent = "";
+            main.remove();
+            //main.classList.add('invisibility');
+            second.classList.add('visibility');
+        }
+    }
+})
